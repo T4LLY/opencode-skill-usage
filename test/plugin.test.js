e70a6518838bd7e300d3c19f77d05ef74c3d52a2
tally <105340992+T4LLY@users.noreply.github.com>
2026-09-10 23:09:53 +0900

    chore: bump version to 0.8.0

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -65,7 +65,7 @@ test.after(async () => {
 })
 
 test("manifest exposes separate server/TUI targets and bounded cache default", () => {
-  assert.equal(manifest.version, "0.7.4")
+  assert.equal(manifest.version, "0.8.0")
   assert.equal(manifest.main, "./src/server.js")
   assert.equal(manifest.exports["./server"].import, "./src/server.js")
   assert.deepEqual(manifest.exports["./server"].config, {
